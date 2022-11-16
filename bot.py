d65f2aaff9c49bc117d5e39ca13e9d25d82a0a82
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2022-11-16 21:24:19 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Salam **{message.from_user.first_name }** \n\n __Mən 🌐 Google Translate Bot 'Am\n Mən Göndərdiyin Mesajı Seçdiyin Dilə Tərcümə Etmək Üçün Proqramlaşdırılmışam\nBaşlamaq Üçün Mənə 1 Mesaj Göndər__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [InlineKeyboardButton("⚡ DİGƏR BOTLARIM" ,url="t.me/menimbitlarim") ],               [InlineKeyboardButton("🇦🇿 OWNER 👨‍💻", url="t.me/sesizKOLGE") ]   ]  ) )
+	await message.reply_text(text =f"Salam **{message.from_user.first_name }** \n\n __Mən 🌐 Google Translate Bot 'Am\n Mən Göndərdiyin Mesajı Seçdiyin Dilə Tərcümə Etmək Üçün Proqramlaşdırılmışam\nBaşlamaq Üçün Mənə 1 Mesaj Göndər__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [InlineKeyboardButton("⚡ DİGƏR BOTLARIM" ,url="t.me/menimbotlarim") ],               [InlineKeyboardButton("🇦🇿 OWNER 👨‍💻", url="t.me/sesizKOLGE") ]   ]  ) )
                   
 
 
